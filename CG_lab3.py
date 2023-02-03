1dbf16c21a47d995a119afc3e66cdfc0b835b9c1
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-02-03 17:33:16 +0000

    added first/last word to end of script

--- CG_lab3.py
+++ CG_lab3.py
@@ -1,11 +1,26 @@
 #prompt user for message
 message = input("Enter a message: ")
 
+#change string permutation
 lower = message.lower()
 upper = message.upper()
 capital = message.capitalize()
 title = message.title()
 
+#split string into list
 list_message = message.split()
-sorted_mess_list = sorted(list_message)
+
+#sort the list of words in alphabetical order
+sorted_list= sorted(list_message)
+
+#print results
+print("Lowercase: " + lower)
+print("Uppercase: " + upper)
+print("Capitalized: " + capital)
+print("Title Case: " + title)
+print("Words: " + str(list_message))
+
+#print first word and last word 
+print("Alphabetic First Word: " + str(sorted_list[0]))
+print("Alphabetic Last Word: " + str(sorted_list[-1]))
 
