81ace23648b53d14532223a5fb14fe79e2f34f32
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-02-03 17:07:33 +0000

    created lan 3 file and began coding task

---
+++ CG_lab3.py
@@ -0,0 +1,11 @@
+#prompt user for message
+message = input("Enter a message: ")
+
+lower = message.lower()
+upper = message.upper()
+capital = message.capitalize()
+title = message.title()
+
+list_message = message.split()
+sorted_mess_list = sorted(list_message)
+
