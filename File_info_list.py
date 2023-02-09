418754c8ff44f1a8accd0e8b453db8c4ea993c7a
Ifeanyi Otuonye (REX TECH) <117672158+ifeanyiro9@users.noreply.github.com>
2023-02-08 19:40:36 -0500

    Update File_info_list.py

--- File_info_list.py
+++ File_info_list.py
@@ -6,6 +6,6 @@ for file in os.listdir(): #loop through all the files in current path
     file_dict = {}
     file_dict['path'] = os.path.realpath(file) #get path name of file
     file_dict['size'] = os.path.getsize(file) #get size of file
-    dict_list.append(file_dict) #add dictioner of file information to list
+    dict_list.append(file_dict) #add dictionary of file information to list
 
 print(*dict_list, sep="\n") #print each dictionary of file information in new line
