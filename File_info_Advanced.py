03aae0215731d0c3ed82f3456ff968d33f83ea48
Ifeanyi Otuonye (REX TECH) <117672158+ifeanyiro9@users.noreply.github.com>
2023-02-08 19:46:03 -0500

    Update File_info_Advanced.py

--- File_info_Advanced.py
+++ File_info_Advanced.py
@@ -5,7 +5,7 @@ dict_list = []
 def extract_info(path = '.'): #function to extract file info, defaulted to present working directory if path is not passed
     dict_list = []
 
-    for root, dirs, files in os.walk(path): #iterate over files in path directory
+    for root, dirs, files in os.walk(path): #recursively iterate over all files/directiories in path directory
         for file_name in files:
             file_dict = {}
             file_dict['path'] = os.path.join(root, file_name) #get path name of file
