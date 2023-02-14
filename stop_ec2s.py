53d1f02a461b49928e267834e5ae609140550f7e
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-02-14 03:17:55 +0000

    Py script to stop all ec2 instances

---
+++ stop_ec2s.py
@@ -0,0 +1,13 @@
+import boto3
+
+ec2_resource = boto3.resource('ec2')
+ec2_client =. boto3.client('ec2')
+
+instances = ec2_resource.instances.all()
+
+for instance in instances:
+    instance_state = instance.state["Name"]
+    
+    if instance_state == 'running':
+        responce = ec2_client.stop_instance(InstanceIds=[instance])
+        
