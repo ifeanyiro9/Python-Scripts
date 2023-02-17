20a9749e2b8ce2e333d9a0465fcdd91c480b452d
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-02-17 14:33:23 +0000

    Readme Edit/File Deletes

--- CG_lab1.py
+++
@@ -1,5 +0,0 @@
-temp_f = float(input("What temperature (in F) would you like converted to Celcius? "))
-temp_c = round((temp_f - 32) * 5/9, 2)
-
-print(str(temp_f) + " F is " + str(temp_c) + " C")
-
--- search_s3bucket.py
+++
@@ -1,6 +0,0 @@
-import boto3
-
-resource = boto3.resource("s3")
-
-for bucket in resource.buckets.all():
-    print(bucket.name)
