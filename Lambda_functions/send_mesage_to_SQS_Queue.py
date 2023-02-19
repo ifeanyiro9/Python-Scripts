e87fa34f49a4acdedfa735cc0b42393d698abbf4
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-02-19 22:36:04 +0000

    Complete lamda Function

--- Lambda_functions/send_mesage_to_SQS_Queue.py
+++ Lambda_functions/send_mesage_to_SQS_Queue.py
@@ -7,12 +7,15 @@ sqs = boto3.resource('sqs', region_name='us-east-1')
 
 def lambda_handler(event, content):
     
-    date_time = current_date_time.strftime("%d/%m/%Y %H:%M:%S")
-    message = ("The current date and time at point of trigger was " + str(date_time) + ".")
+    queue = sqs.get_queue_by_name(QueueName='rextech_queue') #get the queue
+
+    date_time = current_date_time.strftime("%d/%m/%Y %H:%M:%S") #get the current date and time
+    message = ("The current date and time at point of trigger was " + str(date_time) + ".") #concatonate strings to create a custom message
     
-    responce = sqs.send_message(MessageBody = message)
+    
+    responce = queue.send_message(MessageBody = message) #send message to queue
 
     return {
-        "status_Code" : 200,
-        'Message_Sent' : json.dumps(message)
-        }
+        "statusCode": 200,
+        'body': json.dumps(message)
+    }
