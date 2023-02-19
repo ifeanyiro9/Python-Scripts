caa08cacf0e746c55df94e466d75a7ef3f780c89
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-02-19 22:21:14 +0000

    Made progress on function

--- Lambda_functions/send_mesage_to_SQS_Queue.py
+++ Lambda_functions/send_mesage_to_SQS_Queue.py
@@ -1,9 +1,18 @@
 import json
 import boto3
+from datetime import datetime
 
+current_date_time = datetime.now()
 sqs = boto3.resource('sqs', region_name='us-east-1')
 
 def lambda_handler(event, content):
     
+    date_time = current_date_time.strftime("%d/%m/%Y %H:%M:%S")
+    message = ("The current date and time at point of trigger was " + str(date_time) + ".")
     
-    return "success"
+    responce = sqs.send_message(MessageBody = message)
+
+    return {
+        "status_Code" : 200,
+        'Message_Sent' : json.dumps(message)
+        }
